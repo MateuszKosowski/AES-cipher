d964745a3d3a56ab4c90f0b16cba5bde35dd2a7c
NikodemNowak <71512463+NikodemNowak@users.noreply.github.com>
2025-03-25 15:09:44 +0100

    Add key length selection and implement key save/load functionality in AES UI

--- View/src/main/java/org/zespol6/view/AESController.java
+++ View/src/main/java/org/zespol6/view/AESController.java
@@ -1,13 +1,13 @@
 package org.zespol6.view;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.*;
+
 import java.io.*;
 import org.zespol6.aes.AES;
 
 import java.math.BigInteger;
+import java.util.Objects;
 
 public class AESController {
 
@@ -51,23 +51,41 @@ public class AESController {
     TextField keyField;
 
     @FXML
+    RadioButton key128;
+
+    @FXML
+    RadioButton key192;
+
+    @FXML
+    RadioButton key256;
+
+    @FXML
+    ToggleGroup key;
+
+    @FXML
     public void initialize() {
+
+        key.selectedToggleProperty().addListener((observable, oldValue, newValue) -> {
+            keyField.clear();
+        });
 
         AES aes = new AES();
         final javafx.scene.input.Clipboard clipboard = javafx.scene.input.Clipboard.getSystemClipboard();
         final javafx.scene.input.ClipboardContent content = new javafx.scene.input.ClipboardContent();
 
         keyGenButton.setOnAction(e -> {
+            int keyLength = getKeySize();
+            // TODO: Implementacja generowania klucza AES o określonej długości
             aes.generateMainKey();
             keyField.setText(aes.getMainKey().toString(16).toUpperCase());
         });
 
         keySaveButton.setOnAction(e -> {
-            // TODO: Implement key save functionality
+            saveButtonAction(keyField, null, 2);
         });
 
         keyLoadButton.setOnAction(e -> {
-            // TODO: Implement key load functionality
+            loadButtonAction(null, keyField, 2);
         });
 
         encryptButton.setOnAction(e -> {
@@ -86,11 +104,11 @@ public class AESController {
         });
 
         loadEncryptedButton.setOnAction(e -> {
-            loadButtonAction(encryptedDataField, true);
+            loadButtonAction(encryptedDataField, null, 0);
         });
 
         loadDecryptedButton.setOnAction(e -> {
-            loadButtonAction(dataField, false);
+            loadButtonAction(dataField, null, 1);
         });
 
         decryptButton.setOnAction(e -> {
@@ -122,28 +140,27 @@ public class AESController {
         });
 
         saveEncryptedButton.setOnAction(e -> {
-            // Otworzenie okienka dialogowego do wyboru pliku
-            javafx.stage.FileChooser fileChooser = new javafx.stage.FileChooser();
-            fileChooser.setTitle("Zapisz zaszyfrowane dane");
-            fileChooser.getExtensionFilters().addAll(
-                    new javafx.stage.FileChooser.ExtensionFilter("Pliki tekstowe", "*.txt")
-            );
-            File selectedFile = fileChooser.showSaveDialog(null);
-
-            if (selectedFile != null) {
-                try {
-                    java.nio.file.Files.write(selectedFile.toPath(), encryptedDataField.getText().getBytes());
-                } catch (IOException ex) {
-                    encryptedDataField.setText("Error: " + ex.getMessage());
-                }
-            }
+            saveButtonAction(null, encryptedDataField, 0);
         });
     }
 
-    private void loadButtonAction(TextArea textArea, boolean isEncrypted) {
+    private void loadButtonAction(TextArea textArea, TextField textField, int option) {
         // Otworzenie okienka dialogowego do wyboru pliku
         javafx.stage.FileChooser fileChooser = new javafx.stage.FileChooser();
-        fileChooser.setTitle(isEncrypted ? "Wybierz plik z zaszyfrowanymi danymi" : "Wybierz plik z odszyfrowanymi danymi");
+        switch (option) {
+            case 0:
+                fileChooser.setTitle("Wybierz plik z zaszyfrowanymi danymi");
+                break;
+            case 1:
+                fileChooser.setTitle("Wybierz plik z odszyfrowanymi danymi");
+                break;
+            case 2:
+                fileChooser.setTitle("Wybierz plik z kluczem");
+                break;
+            default:
+                fileChooser.setTitle("Wybierz plik");
+                break;
+        }
         fileChooser.getExtensionFilters().addAll(
                 new javafx.stage.FileChooser.ExtensionFilter("Wszystkie pliki", "*.*"),
                 new javafx.stage.FileChooser.ExtensionFilter("Pliki tekstowe", "*.txt")
@@ -153,11 +170,61 @@ public class AESController {
         if (selectedFile != null) {
             try {
                 String contentLoad = new String(java.nio.file.Files.readAllBytes(selectedFile.toPath()));
-                textArea.setText(contentLoad);
+                if (textArea != null) {
+                    textArea.setText(contentLoad);
+                } else {
+                    textField.setText(contentLoad);
+                }
             } catch (IOException ex) {
-                textArea.setText("Error: " + ex.getMessage());
+                if (textArea != null) {
+                    textArea.setText("Error: " + ex.getMessage());
+                } else {
+                    textField.setText("Error: " + ex.getMessage());
+                }
             }
         }
     }
 
+    private void saveButtonAction(TextField textField, TextArea textArea, int option) {
+        // Otworzenie okienka dialogowego do wyboru pliku
+        javafx.stage.FileChooser fileChooser = new javafx.stage.FileChooser();
+        switch (option) {
+            case 0:
+                fileChooser.setTitle("Zapisz zaszyfrowane dane");
+                break;
+            case 2:
+                fileChooser.setTitle("Zapisz klucz");
+                break;
+            default:
+                fileChooser.setTitle("Zapisz plik");
+                break;
+        }
+        fileChooser.getExtensionFilters().addAll(
+                new javafx.stage.FileChooser.ExtensionFilter("Pliki tekstowe", "*.txt")
+        );
+        File selectedFile = fileChooser.showSaveDialog(null);
+
+        if (selectedFile != null) {
+            try {
+                if (textArea != null) {
+                    java.nio.file.Files.write(selectedFile.toPath(), textArea.getText().getBytes());
+                } else {
+                    java.nio.file.Files.write(selectedFile.toPath(), textField.getText().getBytes());
+                }
+            } catch (IOException ex) {
+                Objects.requireNonNullElse(textArea, textField).setText("Error: " + ex.getMessage());
+            }
+        }
+    }
+
+    private int getKeySize() {
+        if (key128.isSelected()) {
+            return 128;
+        } else if (key192.isSelected()) {
+            return 192;
+        } else if (key256.isSelected()) {
+            return 256;
+        }
+        return 128; // Domyślny rozmiar klucza
+    }
 }
